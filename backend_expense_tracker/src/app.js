da764720dfb45b2d0f719585b822efba3c7ae5b9
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-15 19:10:33 +0545

    feat:  Add healthcheck Controller

--- backend_expense_tracker/src/app.js
+++ backend_expense_tracker/src/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 const app = express();
 app.use(cors({
@@ -14,5 +15,8 @@ app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//Routes
+app.use("/api/v1/healthcheck",healthcheckRouter);
 
 export {app};
