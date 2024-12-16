570bf4a78a8bd3001ed3a07d24af4ac622af0593
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-16 16:34:38 +0545

    feat: Configured Multer and Cloudinary For Uploading Invoice Image

--- backend_expense_tracker/src/controller/transaction.controller.js
+++ backend_expense_tracker/src/controller/transaction.controller.js
@@ -3,11 +3,13 @@ import {Category} from "../models/categories.models.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {deleteFromCloudinary} from "../utils/cloudinary.js";
 
 const _id = process.env.USER_ID;
 
 const createTransaction = asyncHandler(async (req, res) => {
-  const { category, amount, type, invoice } = req.body;
+  const { category, amount, type} = req.body;
   
   //Validate required Fields
   if (!amount || !category || !type) {
@@ -24,13 +26,23 @@ const createTransaction = asyncHandler(async (req, res) => {
     throw new apiError(400, "Invalid categroy or type");
   }
 
+  const invoiceLocalPath = req.files?.invoice?.[0]?.path;
+
+  let invoice;
+   try {
+     invoice = await uploadOnCloudinary(invoiceLocalPath);
+     console.log("Invoice uploaded successfully",invoice);
+   } catch (error) {
+    console.log("Error Uploading Invoice",error)
+   }
+
   try {
     const transaction = await Transaction.create({
       userId:_id,
       category,
       type,
       amount,
-      invoice,
+      invoice: invoice.url,
     });
 
     if (!transaction) {
@@ -42,6 +54,10 @@ const createTransaction = asyncHandler(async (req, res) => {
       .json(new apiResponse(201, "Transaction Created Successfully"));
   } catch (error) {
     console.log("Transaction Creation Failed");
+
+    if(invoice){
+      await deleteFromCloudinary(invoice.public_id);
+  }
     throw new apiError(500, "Internal Server Error");
   }
 });
