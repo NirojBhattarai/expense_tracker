e725ace143d59273ae8adda595a01ab1877e0489
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-16 16:51:21 +0545

    ref: Fix Route Issue in Transaction Route

--- backend_expense_tracker/src/routes/transaction.routes.js
+++ backend_expense_tracker/src/routes/transaction.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import {createTransaction} from "../controller/transaction.controller.js";
+import {upload} from "../middlewares/multer.middlewares.js"
 
 const router = Router();
 
-router.route('/create').post(createTransaction);
+router.route('/create').post(
+    upload.fields([
+        {
+            name:"invoice",
+            maxCount:1
+        },
+    ]),createTransaction);
 
 export default router;
