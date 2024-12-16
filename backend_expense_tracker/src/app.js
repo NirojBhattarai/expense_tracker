005f3a896b76f82477c86fb293d71253797879b6
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-16 13:54:42 +0545

    ref: Add Allowed Origins in CORS

--- backend_expense_tracker/src/app.js
+++ backend_expense_tracker/src/app.js
@@ -5,17 +5,30 @@ import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
 
 const app = express();
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials:true
-}
-));
-
+const corsOptions = {
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "*",
+      ];
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        // !origin check allows requests from non-browser clients like Postman
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    optionsSuccessStatus: 200, // For legacy browser support
+  };
+  
+  
 //middlewares
 app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(cors(corsOptions))
 
 //Routes
 app.use("/api/v1/healthcheck",healthcheckRouter);
