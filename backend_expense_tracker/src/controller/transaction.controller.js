68385a9014c4a827a28aaf3eaaff6661e229c88d
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-16 16:06:40 +0545

    feat: Add Create Transaction Function on Transaction Controller

--- backend_expense_tracker/src/controller/transaction.controller.js
+++ backend_expense_tracker/src/controller/transaction.controller.js
@@ -1,25 +1,30 @@
-import Transaction from "../models/transactions.models";
-import Category from "../models/categories.models";
-import { apiError } from "../utils/apiError";
-import { apiResponse } from "../utils/apiResponse";
+import {Transaction} from "../models/transactions.models.js";
+import {Category} from "../models/categories.models.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTransaction = asyncHandler(async (req, res) => {
   const { category, amount, type, invoice } = req.body;
-
+  
   //Validate required Fields
   if (!amount || !category || !type) {
     throw new apiError(400, "All Fields are Required");
   }
 
   //Validate Category
-  const categoryExists = await Category.findOne({ name: category, type });
+  const categoryExists = await Category.findOne({
+    name: new RegExp(`^${category}$`, "i"),
+    type: new RegExp(`^${type}$`, "i"),
+  });
 
   if (!categoryExists) {
     throw new apiError(400, "Invalid categroy or type");
   }
+
   try {
     const transaction = await Transaction.create({
-      userId: req.user._id,
+      userId: "675ee8b8eefd45e72660c0d4",
       category,
       type,
       amount,
