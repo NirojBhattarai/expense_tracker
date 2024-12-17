5b49208092f98ee289a15a76959e4d76ae1f6930
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-17 10:11:00 +0545

    Update Backend to Handle User Cookies

--- backend_expense_tracker/src/routes/user.routes.js
+++ backend_expense_tracker/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import {loginUser, registerUser} from "../controller/user.controller.js"
+import {loginUser, refreshToken, registerUser} from "../controller/user.controller.js"
 
 const router = Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/token').post(refreshToken);
 
 export default router
