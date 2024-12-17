491b6702b7a2e10a770d7c0b3edd16ae289d57bf
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-17 15:01:05 +0545

    fix: Logout Bug Fix

--- frontend_expense_tracker/src/components/Header/Header.jsx
+++ frontend_expense_tracker/src/components/Header/Header.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext.js";
 
 function Header() {
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/v1/users/logout");
+
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      logout();
+      navigate("/login");
+      
+    } catch (error) {
+      console.error("Logout failed", error.response?.data?.message || error.message);
+    }
+  };
+
   return (
     <header className="bg-cyan-950 shadow-lg py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
@@ -9,14 +29,11 @@ function Header() {
             ExpenseTracker
           </span>
         </div>
-        <nav className="flex space-x-6">
-          <a
-            href="#logout"
-            className="text-[#3d52a0] bg-[#ede8f5] px-4 py-2 rounded-lg hover:bg-[#adbbda] hover:text-white font-semibold transition"
-          >
+        <button className="flex space-x-6" onClick={handleLogout}>
+          <span className="text-[#3d52a0] bg-[#ede8f5] px-4 py-2 rounded-lg hover:bg-[#adbbda] hover:text-white font-semibold transition cursor-pointer">
             Logout
-          </a>
-        </nav>
+          </span>
+        </button>
       </div>
     </header>
   );
